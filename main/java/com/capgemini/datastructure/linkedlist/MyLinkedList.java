029b846e2dd9208fffaa83d77989608ef0e50b57
unknown <rachitshukla08@gmail.com>
2020-10-07 22:14:59 +0530

    Added: searchAndRemove and size method to MyLinkedList

--- main/java/com/capgemini/datastructure/linkedlist/MyLinkedList.java
+++ main/java/com/capgemini/datastructure/linkedlist/MyLinkedList.java
@@ -3,6 +3,7 @@ package com.capgemini.datastructure.linkedlist;
 public class MyLinkedList<K> {
 	public INode head;
 	public INode tail;
+	public int size=0;
 	
 	public MyLinkedList() {
 		this.head = null;
@@ -10,15 +11,16 @@ public class MyLinkedList<K> {
 	}
 
 	public void add(INode newNode) {
-	if(this.tail == null)
-		this.tail = newNode;
-	if(this.head == null)
-		this.head = newNode;
-	else {
-		INode tempNode = this.head;
-		this.head = newNode;
-		this.head.setNext(tempNode);
-		}
+		if(this.tail == null)
+			this.tail = newNode;
+		if(this.head == null)
+			this.head = newNode;
+		else {
+			INode tempNode = this.head;
+			this.head = newNode;
+			this.head.setNext(tempNode);
+			}
+		size++;
 	}
 	
 	public void append(INode newNode) {
@@ -30,6 +32,7 @@ public class MyLinkedList<K> {
 			this.tail.setNext(newNode);
 			this.tail = newNode;
 		}
+		size++;
 	}
 
 	public void printMyNodes() {
@@ -49,11 +52,13 @@ public class MyLinkedList<K> {
 		INode tempNode = myNode.getNext();
 		myNode.setNext(newNode);
 		newNode.setNext(tempNode);
+		size++;
 	}
 	
 	public INode pop() {
 		INode tempNode = this.head;
 		this.head = head.getNext();
+		size--;
 		return tempNode;
 	}
 	
@@ -64,6 +69,7 @@ public class MyLinkedList<K> {
 		}
 		this.tail = tempNode;
 		tempNode=tempNode.getNext();
+		size--;
 		return tempNode;
 	}
 	
@@ -91,6 +97,25 @@ public class MyLinkedList<K> {
 		INode tempNode2 = tempNode.getNext();
 		tempNode.setNext(newNode);
 		newNode.setNext(tempNode2);
+		size++;
 		return tempNode;
+	}
+	
+	public INode searchAndRemove(K key) {
+		INode tempNode = head;
+		while(tempNode!=null&&tempNode.getNext()!=null) {
+			if(tempNode.getNext().getKey().equals(key)) {
+				break;
+			}
+			else 
+				tempNode = tempNode.getNext();	
+		}
+		tempNode.setNext(tempNode.getNext().getNext());
+		size--;
+		return tempNode;
+	}
+	
+	public int size() {
+		return size;
 	}
 }
