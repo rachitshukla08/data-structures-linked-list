ac3018b1b406e754ddfc6ffc9c9ee4aadceb86aa
unknown <rachitshukla08@gmail.com>
2020-10-07 20:58:55 +0530

    Corrected test cases

--- test/java/com/capgemini/datastructure/linkedlist/LinkedListTest.java
+++ test/java/com/capgemini/datastructure/linkedlist/LinkedListTest.java
@@ -16,6 +16,7 @@ public class LinkedListTest {
 		myLinkedList.printMyNodes();
 		boolean result = myLinkedList.head.equals(myThirdNode) && myLinkedList.head.getNext().equals(mySecondNode)
 				&& myLinkedList.tail.equals(myFirstNode);
+		assertEquals(true,result);
 	}
 	
 	@Test
@@ -28,12 +29,28 @@ public class LinkedListTest {
 		myLinkedList.add(mySecondNode);
 		myLinkedList.add(myThirdNode);
 		myLinkedList.printMyNodes();
-		boolean result = myLinkedList.head.equals(myFirstNode) && myLinkedList.head.getNext().equals(mySecondNode)
-				&& myLinkedList.tail.equals(myThirdNode);
+		boolean result = myLinkedList.head.equals(myThirdNode) && myLinkedList.head.getNext().equals(mySecondNode)
+				&& myLinkedList.tail.equals(myFirstNode);
+		assertEquals(true,result);
 	}
 	
 	@Test
 	public void given3Numbers_WhenInsertingSecondNumber_ShouldBeAferFirstAndBeforeThird() {
+		MyNode<Integer> myFirstNode = new MyNode<Integer>(56);
+		MyNode<Integer> mySecondNode = new MyNode<Integer>(30);
+		MyNode<Integer> myThirdNode = new MyNode<Integer>(70);
+		MyLinkedList myLinkedList = new MyLinkedList();
+		myLinkedList.add(myFirstNode);
+		myLinkedList.append(myThirdNode);
+		myLinkedList.insert(myFirstNode, mySecondNode);
+		myLinkedList.printMyNodes();
+		boolean result = myLinkedList.head.equals(myFirstNode) && myLinkedList.head.getNext().equals(mySecondNode)
+				&& myLinkedList.tail.equals(myThirdNode);
+		assertEquals(true,result);
+	}
+	
+/*	@Test
+	public void given3Numbers_WhenPopping_ShouldRemoveFirstNumber() {
 		MyNode<Integer> myFirstNode = new MyNode<Integer>(56);
 		MyNode<Integer> mySecondNode = new MyNode<Integer>(30);
 		MyNode<Integer> myThirdNode = new MyNode<Integer>(70);
@@ -45,6 +62,7 @@ public class LinkedListTest {
 		boolean result = myLinkedList.head.equals(myFirstNode) && myLinkedList.head.getNext().equals(mySecondNode)
 				&& myLinkedList.tail.equals(myThirdNode);
 	}
+	*/
 
 
 
