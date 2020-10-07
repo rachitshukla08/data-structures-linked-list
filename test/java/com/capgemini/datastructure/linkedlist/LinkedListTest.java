5decb30d137616bf2b4647ea7f9bc61db3ab914a
unknown <rachitshukla08@gmail.com>
2020-10-07 21:12:05 +0530

    Added: pop method to MyLinkedList to remove first element

--- test/java/com/capgemini/datastructure/linkedlist/LinkedListTest.java
+++ test/java/com/capgemini/datastructure/linkedlist/LinkedListTest.java
@@ -49,20 +49,20 @@ public class LinkedListTest {
 		assertEquals(true,result);
 	}
 	
-/*	@Test
+	@Test
 	public void given3Numbers_WhenPopping_ShouldRemoveFirstNumber() {
 		MyNode<Integer> myFirstNode = new MyNode<Integer>(56);
 		MyNode<Integer> mySecondNode = new MyNode<Integer>(30);
 		MyNode<Integer> myThirdNode = new MyNode<Integer>(70);
 		MyLinkedList myLinkedList = new MyLinkedList();
 		myLinkedList.add(myFirstNode);
+		myLinkedList.add(mySecondNode);
 		myLinkedList.add(myThirdNode);
-		myLinkedList.insert(myFirstNode, mySecondNode);
 		myLinkedList.printMyNodes();
-		boolean result = myLinkedList.head.equals(myFirstNode) && myLinkedList.head.getNext().equals(mySecondNode)
-				&& myLinkedList.tail.equals(myThirdNode);
+		MyNode<Integer> pop = (MyNode<Integer>) myLinkedList.pop();
+		assertEquals(mySecondNode, pop);
 	}
-	*/
+	
 
 
 
