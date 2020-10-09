f44042f1c27e8716a4a522f7f01418149a574ee8
unknown <rachitshukla08@gmail.com>
2020-10-09 15:56:11 +0530

    Added: dequeue method to MyQueue and test case for the same

--- test/java/com/capgemini/datastructure/queue/MyQueueTest.java
+++ test/java/com/capgemini/datastructure/queue/MyQueueTest.java
@@ -22,4 +22,14 @@ public class MyQueueTest {
 		myQueue.printQueue();
 		assertEquals((int)myThirdNode.getKey(), 70);
 	}
+	
+	@Test
+	public void given3Numbers_dequeued_ShouldRemoveFirstNode() {
+		MyQueue myQueue = new MyQueue();
+		MyNode<Integer> myFirstNode = new MyNode<Integer>(56);
+		MyNode<Integer> mySecondNode = new MyNode<Integer>(30);
+		MyNode<Integer> myThirdNode = new MyNode<Integer>(70);
+		myQueue.dequeue();
+		assertEquals((int)myFirstNode.getKey(), 56);
+	}
 }
